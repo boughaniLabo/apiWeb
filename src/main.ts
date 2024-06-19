027b15d7677130255c9ed8e02f74b7866812ddaa
Boughani <abdelghani.demmouche.prof@gmail.com>
2024-06-19 13:42:54 +0100

    add thios

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as path from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync(path.join(__dirname, '../.certs/key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, '../.certs/cert.pem')),
+
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   app.enableCors();
   await app.listen(3000);
 }
